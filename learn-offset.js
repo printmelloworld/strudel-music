989968c7e718261e5211b7d8be32d6c73b549135
print('Mello World!') <printmelloworld@proton.me>
2025-10-24 19:02:30 -0300

    Create learn-offset.js

---
+++ learn-offset.js
@@ -0,0 +1,12 @@
+// "Learn: Pattern offset"
+// @by print('Mello World!')
+
+setcpm(140/4)
+
+$: stack(
+s("bd:4").beat("0,4,8,12", 16),
+s("bd:4").beat("3.3,11.5", 16).gain(.3),
+// s("sd:3").beat("4,12", 16),
+// s("hh!16").dec(.1).gain(rand.range(.2,.4)),
+// s("cb").beat("3,4,6,9",16).delay(.66).gain(.1),
+)
